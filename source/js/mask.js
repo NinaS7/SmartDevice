cc04b0eef36e8ee0d30be8a5a24ffc55b485ec2a
Нина Стафеева <rugacheva@mail.ru>
2022-05-19 11:15:55 +0300

    проект на код-ревью

--- source/js/mask.js
+++ source/js/mask.js
@@ -1,6 +1,6 @@
 const getMask = () => {
   document.getElementById('phone').addEventListener('input', function (e) {
-    const x = e.target.value.replace(/\D/g, '').match(/\+(\d)(\d{0,3})(\d{0,3})(\d{0,4})/);
+    const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
     e.target.value = '+7(' + x[1] + ')' + x[2] + (x[3] ? '-' + x[3] : '');
   });
 };
